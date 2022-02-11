2dd0893c2b68115d31112248f3024ee0a41d7b06
Mickey <devok121212@gmail.com>
2022-02-11 11:04:23 +0900

    change logic

--- api/views.py
+++ api/views.py
@@ -3,25 +3,33 @@ from django.http import HttpResponse, JsonResponse
 import time
 from binance.websocket.spot.websocket_client import SpotWebsocketClient as Client
 
+price_btcusdt = 0
+price_ethusdt = 0
 
 def index(request, symbol):
-    global price
+    global price_btcusdt, price_ethusdt
+    
+    if symbol == 'btcusdt':
+        return JsonResponse({'price': price_btcusdt})
+    elif symbol == 'ethusdt':
+        return JsonResponse({'price': price_ethusdt})
+    else:
+        return JsonResponse({'price': 0})
 
-    # if symbol in price:
-    #     return JsonResponse({'price': price[symbol]})
-    # else
-    #     return JsonResponse({'price': 0})
-    return JsonResponse({'price': price})
+
+def message_handler_btcusdt(message):
+    global price_btcusdt
+    price_btcusdt = message
 
 
-def message_handler(message):
-    global price
-
-    price = message
+def message_handler_ethusdt(message):
+    global price_ethusdt
+    price_ethusdt = message
 
 
 price = {}
 my_client = Client()
 my_client.start()
 
-my_client.ticker(id=1, callback=message_handler)
+my_client.ticker(symbol="btcusdt", id=1, callback=message_handler_btcusdt)
+my_client.ticker(symbol="ethusdt", id=2, callback=message_handler_ethusdt)
